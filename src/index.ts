46fc4c447b0456a326fa9864c820b3e12183373d
tom <toms@live.jp>
2022-08-20 06:40:41 +0900

    platform() is added in preloadObject

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ const { ipcRenderer } = require("electron");
 export const preloadObject = {
   os: {
     homedir: () => ipcRenderer.invoke("electronade-os:homedir"),
+    platform: () => ipcRenderer.invoke("electronade-os:platform"),
     tmpdir: () => ipcRenderer.invoke("electronade-os:tmpdir")
   }
 };
