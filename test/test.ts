360577a07be216e196258465a7d655ab121953b2
tom <toms@live.jp>
2022-08-20 06:42:12 +0900

    handle test for platform is added

--- test/test.ts
+++ test/test.ts
@@ -2,7 +2,7 @@ import { describe, it } from "mocha";
 import { strict as assert } from "assert";
 
 import { handles } from "../src/handles";
-import { homedir, tmpdir } from "os";
+import { homedir, platform, tmpdir } from "os";
 
 describe("handles", () => {
   it("electronade-os:homedir eventName exists", () => {
@@ -23,6 +23,26 @@ describe("handles", () => {
         .handler(),
       homedir()
     )
+  });
+
+  it("electronade-os:platform eventName exists", () => {
+    assert(
+      handles.find(
+        ({ eventName }) =>
+          eventName === "electronade-os:platform"
+      )
+    );
+  });
+
+  it("electronade-os:platform handler", () => {
+    assert.equal(
+      (handles.find(
+        ({ eventName }) =>
+          eventName === "electronade-os:platform"
+      ) as { handler: Function })
+        .handler(),
+      platform()
+    );
   });
 
   it("electronade-os:tmpdir eventName exists", () => {
