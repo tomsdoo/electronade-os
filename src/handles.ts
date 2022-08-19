617f526c63a0f9f51da887bb6c23885dd14d8d66
tom <toms@live.jp>
2022-08-20 06:40:05 +0900

    handle for platform is added

--- src/handles.ts
+++ src/handles.ts
@@ -1,9 +1,13 @@
-import { homedir, tmpdir } from "os";
+import { homedir, platform, tmpdir } from "os";
 
 export const handles = [
   {
     eventName: "electronade-os:homedir",
     handler: () => homedir()
+  },
+  {
+    eventName: "electronade-os:platform",
+    handler: () => platform()
   },
   {
     eventName: "electronade-os:tmpdir",
