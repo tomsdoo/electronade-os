e23f795043f38d11220f2dbf464437a58b258145
tom <toms@live.jp>
2022-08-21 08:34:34 +0900

    test for integration of preload and handles

---
+++ test/test.ts
@@ -0,0 +1,37 @@
+import { describe, it } from "mocha";
+import { strict as assert } from "assert";
+
+import { handles, preloadObject } from "../src/";
+
+const ipcRenderer: {
+  invoke: (eventName: string, ...args: any[]) => Promise<any>;
+} = {
+  invoke: (eventName: string) => Promise.resolve(eventName)
+};
+
+const handleDictionary = Object.fromEntries(
+  handles.map(({ eventName, handler }) => [eventName, handler])
+);
+
+describe("preloadObject to handles", () => {
+  it("preloadObject.os.homedir()", async () => {
+    assert(
+      await eval(preloadObject.os.homedir.toString())()
+        in handleDictionary
+    );
+  });
+
+  it("preloadObject.os.platform()", async () => {
+    assert(
+      await eval(preloadObject.os.platform.toString())()
+        in handleDictionary
+    );
+  });
+
+  it("preloadObject.os.tmpdir()", async () => {
+    assert(
+      await eval(preloadObject.os.tmpdir.toString())()
+        in handleDictionary
+    );
+  });
+});
